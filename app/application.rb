545d19dfe2454acf42b9840c01a187347fd781ed
Marc Poppy <poppy.marc@gmail.com>
2020-02-01 23:51:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,12 +13,12 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
-      if @@cart.!empty?
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
         @@cart.each do |c|
         resp.write "#{c}"
         end
-      else
-        resp.write "Your cart is empty"
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
